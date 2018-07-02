40feb7fd72f77377803359bcf1c10245f49a2c6a
Renaud Schleck <renaud.schleck@free.fr>
2018-07-02 22:11:17 +0200

    ajout de gitignore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,6 @@ export a=$(date +%d/%m/%Y)
 cp --backup=numbered index.html backup/
 cat index.html | sed -e "s;<p>Dernière mise à jour : .*<\/p>;<p>Dernière mise à jour : "$(date +%d/%m/%Y)"<\/p>;g" > temp.html
 mv temp.html index.html
-bitballoon deploy
+cp -Lr fichiers_liens/* fichiers/
+cp -Lr info_liens/* info/
+netlify deploy
