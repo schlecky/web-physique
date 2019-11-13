7630860aa9720aa7dbde43eee38f928e3ef445f2
Renaud Schleck <renaud.schleck@free.fr>
2019-11-13 10:32:20 +0100

    13/11/2019

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,7 @@ export a=$(date +%d/%m/%Y)
 cp --backup=numbered index.html backup/
 cat index.html | sed -e "s;<p>Dernière mise à jour : .*<\/p>;<p>Dernière mise à jour : "$(date +%d/%m/%Y)"<\/p>;g" > temp.html
 mv temp.html index.html
-cp -Lr fichiers_liens/* fichiers/
-cp -Lr info_liens/* info/
+scripts/maj_liens.sh
 #netlify deploy
 git commit -a -m $(date +%d/%m/%Y)
 git push
